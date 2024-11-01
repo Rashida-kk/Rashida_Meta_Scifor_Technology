a151c6121248b30f1602582201787a24e20fefbb
Rashida-kk <157975077+Rashida-kk@users.noreply.github.com>
2024-11-01 20:59:58 +0530

    Update miniproject_1.py

--- Projects/miniproject_1.py
+++ Projects/miniproject_1.py
@@ -11,7 +11,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 import plotly.express as px
 import requests
